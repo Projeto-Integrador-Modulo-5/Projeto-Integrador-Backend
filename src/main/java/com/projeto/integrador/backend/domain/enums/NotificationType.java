04c1d27ab22189f6192bfe5aed68e12a9c9b69c6
Victro Hugo <vhaporfiro@gmail.com>
2026-05-18 20:48:21 -0300

    Adiciona novos tipos de notificação e status de pedido ao enum NotificationType e OrderStatus

--- src/main/java/com/projeto/integrador/backend/domain/enums/NotificationType.java
+++ src/main/java/com/projeto/integrador/backend/domain/enums/NotificationType.java
@@ -2,6 +2,8 @@ package com.projeto.integrador.backend.domain.enums;
 
 public enum NotificationType {
     ORDER_CREATED,
+    ORDER_PROCESSING,
     ORDER_SHIPPED,
-    ORDER_DELIVERED
+    ORDER_DELIVERED,
+    ORDER_CANCELLED
 }
